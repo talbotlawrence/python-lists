2d3589a3c6d80dc4eb523da278dfe47d3abbbb2b
Talbot <talbotlawrence75@gmail.com>
2017-04-13 20:12:26 -0500

    studied and slightly changed

--- planets.py
+++ planets.py
@@ -3,21 +3,21 @@ planet_list = ["Mercury", "Mars"]
 new_planets = ["Jupiter", "Saturn"]
 for planet in new_planets:
 	planet_list.append(planet)
-# print(planet_list)
+print(planet_list)
 
 planet_list.extend(['Uranus','Neptune'])
-# print(planet_list)
+print(planet_list)
 
 planet_list.insert(1, 'Earth')
 planet_list.insert(1, 'Venus')
 planet_list.append('Pluto')
-# print(planet_list)
+print(planet_list)
 
 rocky_planets = planet_list[0:4]
-# print(rocky_planets)
+print(rocky_planets)
 
 del planet_list[-1]
-# print(planet_list)
+print(planet_list)
 
 spacecraft_and_planets = [
 ('Ford','Mercury'),
